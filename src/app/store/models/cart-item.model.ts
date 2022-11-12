2803f258ae3f092cd11cb8c419ef754676367498
ikcilrep <ikcilrep@outlook.com>
2022-11-12 18:47:37 +0100

    Use NgRx selectors instead of class methods

--- src/app/store/models/cart-item.model.ts
+++ src/app/store/models/cart-item.model.ts
@@ -1,13 +1,6 @@
 import { Product } from 'src/app/db';
 
-export default class CartItem {
-    constructor(public product: Product, public amount: number) { }
-
-    public get price(): number {
-        return this.amount * this.product.price;
-    }
-
-    public get displayablePrice(): string {
-        return this.price.toFixed(2) + 'zł';
-    }
+export default interface CartItem {
+    readonly product: Product;
+    readonly amount: number;
 }
