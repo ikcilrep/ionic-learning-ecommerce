72df31e9f7a22a56b14b39704f078624bb07ce78
ikcilrep <ikcilrep@outlook.com>
2022-11-20 16:41:20 +0100

    Remove an unused selector

--- src/app/selectors/login.selectors.ts
+++ src/app/selectors/login.selectors.ts
@@ -4,5 +4,3 @@ import * as fromLogin from '../reducers/login.reducer';
 export const selectLoginState = createFeatureSelector<fromLogin.State>(
   fromLogin.loginFeatureKey
 );
-
-export const selectIsLoggedIn = createSelector(selectLoginState, (state) => state.isLoggedIn);
