b1244695528276b3f2c1eb40c4c1b1d25363133e
ikcilrep <ikcilrep@outlook.com>
2022-11-24 22:14:36 +0100

    Write unit tests for CartSelectors

--- src/app/selectors/cart.selectors.spec.ts
+++ src/app/selectors/cart.selectors.spec.ts
@@ -1,12 +1,47 @@
+import { products } from '../db';
 import * as fromCart from '../reducers/cart.reducer';
-import { selectCartState } from './cart.selectors';
+import { selectCartItems, selectCartState, selectCartTotalPrice, selectNumberOfItems } from './cart.selectors';
 
 describe('Cart Selectors', () => {
-  it('should select the feature state', () => {
-    const result = selectCartState({
-      [fromCart.cartFeatureKey]: fromCart.initialState
+  const items = [{ product: products[0], amount: 1 }, { product: products[1], amount: 1 }];
+  describe('selectCartState', () => {
+    it('should select the feature state', () => {
+      const result = selectCartState({
+        [fromCart.cartFeatureKey]: fromCart.initialState
+      });
+
+      expect(result).toEqual(fromCart.initialState);
+    });
+  });
+
+  describe('selectCartItems', () => {
+    it('should select the cart items', () => {
+      const result = selectCartItems.projector({ items });
+
+      expect(result).toEqual(jasmine.arrayWithExactContents(items));
     });
 
-    expect(result).toEqual(fromCart.initialState);
+    it('should return items sorted ascendingly alphabetically by product names', () => {
+      const result = selectCartItems.projector({ items });
+
+      expect(result).toEqual([items[1], items[0]]);
+    });
+  });
+
+  describe('selectCartTotalPrice', () => {
+    it('should select the sum of item prices', () => {
+      const result = selectCartTotalPrice.projector({ items });
+
+      const expectedPrice = items[0].product.price + items[1].product.price;
+
+      expect(result).toBe(expectedPrice);
+    });
+  });
+
+  describe('selectNumberOfItems', () => {
+    it('should select the number of items', () => {
+      const result = selectNumberOfItems.projector({ items });
+      expect(result).toBe(2);
+    });
   });
 });
