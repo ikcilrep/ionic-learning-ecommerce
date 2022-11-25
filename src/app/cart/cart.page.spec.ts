4d9962968c7f5a1053152ec4ac70d5436afc921f
ikcilrep <ikcilrep@outlook.com>
2022-11-25 17:38:24 +0100

    Write unit tests for cart page

--- src/app/cart/cart.page.spec.ts
+++ src/app/cart/cart.page.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { Router } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 import { provideMockStore } from '@ngrx/store/testing';
 import * as fromCart from '../reducers/cart.reducer';
@@ -8,15 +9,17 @@ import { CartPage } from './cart.page';
 describe('CartPage', () => {
   let component: CartPage;
   let fixture: ComponentFixture<CartPage>;
+  let routerSpyObj: any;
 
   beforeEach(waitForAsync(() => {
+    routerSpyObj = jasmine.createSpyObj('Router', ['navigate']);
     TestBed.configureTestingModule({
       declarations: [CartPage],
       imports: [IonicModule.forRoot()],
       providers: [provideMockStore({
         initialState: { cart: fromCart.initialState },
-        selectors: [{ selector: selectCartTotalPrice, value: 0 }, { selector: selectCartItems, value: [] }]
-      })],
+        selectors: [{ selector: selectCartTotalPrice, value: 0 }, { selector: selectCartItems, value: [] }],
+      }), { provide: Router, useValue: routerSpyObj }],
     }).compileComponents();
 
     fixture = TestBed.createComponent(CartPage);
@@ -26,5 +29,12 @@ describe('CartPage', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('goToHome', () => {
+    it('should navigate to /home', () => {
+      component.goToHome();
+      expect(routerSpyObj.navigate).toHaveBeenCalledOnceWith(['/home']);
+    });
   });
 });
