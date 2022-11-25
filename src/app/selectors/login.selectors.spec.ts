09d17471c5249d447a70629142a336c6a1c77a6e
ikcilrep <ikcilrep@outlook.com>
2022-11-25 19:45:18 +0100

    Test login selectors using a mock store

--- src/app/selectors/login.selectors.spec.ts
+++ src/app/selectors/login.selectors.spec.ts
@@ -1,7 +1,12 @@
+import { TestBed } from '@angular/core/testing';
+import { provideMockStore } from '@ngrx/store/testing';
 import * as fromLogin from '../reducers/login.reducer';
 import { selectLoginState } from './login.selectors';
 
 describe('Login Selectors', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({ providers: [provideMockStore()] });
+  });
   it('should select the feature state', () => {
     const result = selectLoginState({
       [fromLogin.loginFeatureKey]: fromLogin.initialState
