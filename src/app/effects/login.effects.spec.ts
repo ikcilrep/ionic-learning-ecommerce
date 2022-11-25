39b43f707e6847bfd13258e3820521be9d036598
ikcilrep <ikcilrep@outlook.com>
2022-11-25 19:45:09 +0100

    Write unit tests for login effects

--- src/app/effects/login.effects.spec.ts
+++ src/app/effects/login.effects.spec.ts
@@ -1,23 +1,25 @@
 import { TestBed } from '@angular/core/testing';
 import { provideMockActions } from '@ngrx/effects/testing';
 import { provideMockStore } from '@ngrx/store/testing';
-import { Store } from '@ngrx/store';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
+import { autoLogInFailure, autoLogInSuccess, logInIfRemembered, saveLoginData } from '../actions/login.actions';
 import StorageService from '../providers/storage.service';
+import { selectLoginState } from '../selectors/login.selectors';
 
 import { LoginEffects } from './login.effects';
 
 describe('LoginEffects', () => {
   let actions$: Observable<any>;
   let effects: LoginEffects;
-
+  let storageServiceSpyObj: any;
+  const email = 'user@example.com';
   beforeEach(() => {
-    const storageServiceSpyObj = jasmine.createSpyObj('StorageService', ['set', 'get']);
+    storageServiceSpyObj = jasmine.createSpyObj('StorageService', ['set', 'get']);
     TestBed.configureTestingModule({
       providers: [
         LoginEffects,
         provideMockActions(() => actions$),
-        provideMockStore(),
+        provideMockStore({ selectors: [{ selector: selectLoginState, value: { email, isLoggedIn: true, isLoaded: true } }] }),
         { provide: StorageService, useValue: storageServiceSpyObj }
       ]
     });
@@ -27,5 +29,35 @@ describe('LoginEffects', () => {
 
   it('should be created', () => {
     expect(effects).toBeTruthy();
+  });
+
+  describe('logInIfRemembered', () => {
+    it('should return autoLoginFailure, if email is not in storage', (done: DoneFn) => {
+      actions$ = of(logInIfRemembered());
+      storageServiceSpyObj.get.and.returnValue(null);
+      effects.logInIfRemembered$.subscribe(action => {
+        expect(action).toEqual(autoLogInFailure());
+        done();
+      });
+    });
+
+    it('should return autoLoginSuccess with the given email, if email is in storage', (done: DoneFn) => {
+      actions$ = of(logInIfRemembered());
+      storageServiceSpyObj.get.and.returnValue(email);
+      effects.logInIfRemembered$.subscribe(action => {
+        expect(action).toEqual(autoLogInSuccess({ email }));
+        done();
+      });
+    });
+  });
+
+  describe('saveLoginData', () => {
+    it('should save the provided email in storage', (done: DoneFn) => {
+      actions$ = of(saveLoginData());
+      effects.saveLoginData$.subscribe(() => {
+        expect(storageServiceSpyObj.set).toHaveBeenCalledOnceWith('email', email);
+        done();
+      });
+    });
   });
 });
