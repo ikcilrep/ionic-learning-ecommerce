dd0f0a322fb5182e7ea99b38940a0ac8a15a9204
ikcilrep <ikcilrep@outlook.com>
2022-12-03 18:33:41 +0100

    Write unit test for CommentContentComponent

--- src/app/src/app/home/product-page/comment-content/comment-content.component.spec.ts
+++ src/app/src/app/home/product-page/comment-content/comment-content.component.spec.ts
@@ -1,24 +1,32 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
+import * as db from 'src/app/db';
+import UserComment from 'src/app/models/user-comment.model';
 
 import { CommentContentComponent } from './comment-content.component';
 
 describe('CommentContentComponent', () => {
   let component: CommentContentComponent;
   let fixture: ComponentFixture<CommentContentComponent>;
+  const comment: UserComment = db.comments[0];
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      declarations: [ CommentContentComponent ],
-      imports: [IonicModule.forRoot()]
+      declarations: [CommentContentComponent],
+      imports: [IonicModule.forRoot()],
     }).compileComponents();
 
     fixture = TestBed.createComponent(CommentContentComponent);
     component = fixture.componentInstance;
+    component.comment = db.comments[0];
     fixture.detectChanges();
   }));
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should find a user with id given in comment', () => {
+    expect(component.user.id).toBe(comment.userId);
   });
 });
