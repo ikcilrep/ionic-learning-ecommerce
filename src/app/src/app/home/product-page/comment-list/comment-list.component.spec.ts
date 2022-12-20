c89a3c4065b81af390df24113a55d32c083c53fe
ikcilrep <ikcilrep@outlook.com>
2022-12-20 22:27:40 +0100

    Write a unit test for loading comments on a page

--- src/app/src/app/home/product-page/comment-list/comment-list.component.spec.ts
+++ src/app/src/app/home/product-page/comment-list/comment-list.component.spec.ts
@@ -1,7 +1,9 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
-import { provideMockStore } from '@ngrx/store/testing';
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
+import { State } from 'src/app/reducers';
 import * as fromComment from 'src/app/reducers/comment.reducer';
+import * as CommentActions from 'src/app/actions/comment.actions';
 
 import { CommentListComponent } from './comment-list.component';
 
@@ -9,6 +11,8 @@ describe('CommentListComponent', () => {
   const productId = 0;
   let component: CommentListComponent;
   let fixture: ComponentFixture<CommentListComponent>;
+  let mockStore: MockStore<State>;
+  let mockStoreDispatchSpy: jasmine.Spy;
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
@@ -16,6 +20,9 @@ describe('CommentListComponent', () => {
       imports: [IonicModule.forRoot()],
       providers: [provideMockStore({ initialState: { [fromComment.commentFeatureKey]: fromComment.initialState } })]
     }).compileComponents();
+
+    mockStore = TestBed.inject(MockStore<State>);
+    mockStoreDispatchSpy = spyOn(mockStore, 'dispatch');
 
     fixture = TestBed.createComponent(CommentListComponent);
     component = fixture.componentInstance;
@@ -25,5 +32,9 @@ describe('CommentListComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should load comments', () => {
+    expect(mockStoreDispatchSpy).toHaveBeenCalledWith(CommentActions.loadComments({ productId }));
   });
 });
