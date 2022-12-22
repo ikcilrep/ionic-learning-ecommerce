2f8ccdd95a052cee5554e6d1049db8d6d0b1631a
ikcilrep <ikcilrep@outlook.com>
2022-12-22 22:13:19 +0100

    Write unit tests for loadComments effect

--- src/app/effects/comment.effects.spec.ts
+++ src/app/effects/comment.effects.spec.ts
@@ -1,12 +1,21 @@
 import { TestBed } from '@angular/core/testing';
 import { provideMockActions } from '@ngrx/effects/testing';
-import { Observable } from 'rxjs';
+import axios from 'axios';
+import { Observable, of } from 'rxjs';
+import { serverAddress } from 'src/app-config';
+import { loadComments, loadCommentsFailure, loadCommentsSuccess } from '../actions/comment.actions';
 
 import { CommentEffects } from './comment.effects';
 
 describe('CommentEffects', () => {
   let actions$: Observable<any>;
   let effects: CommentEffects;
+  let axiosGetSpy: jasmine.Spy;
+  let axiosPostSpy: jasmine.Spy;
+  const productId = 0;
+  const comments = [{ id: '0', userId: 2, productId, text: 'Testing comment 1', createdAt: undefined },
+  { id: '1', userId: 3, productId, text: 'Testing comment 2', createdAt: undefined },
+  ];
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -16,10 +25,55 @@ describe('CommentEffects', () => {
       ]
     });
 
+    axiosGetSpy = spyOn(axios, 'get');
+    axiosPostSpy = spyOn(axios, 'post');
+
     effects = TestBed.inject(CommentEffects);
   });
 
   it('should be created', () => {
     expect(effects).toBeTruthy();
+  });
+
+  describe('loadComments', () => {
+    it('should make a get request to the api with the product id', (done: DoneFn) => {
+      actions$ = of(loadComments({ productId }));
+      effects.loadComments$.subscribe(() => {
+        expect(axiosGetSpy).toHaveBeenCalledOnceWith(`${serverAddress}/api/comments/${productId}`);
+        done();
+      });
+    });
+
+    it('should return loadCommentsSuccess with returned comments, if the request was successful', (done: DoneFn) => {
+      actions$ = of(loadComments({ productId }));
+      axiosGetSpy.and.returnValue({ data: comments, status: 200 });
+      effects.loadComments$.subscribe((action) => {
+        expect(action).toEqual(loadCommentsSuccess({
+          comments,
+          productId: 0
+        }));
+        done();
+      });
+    });
+
+    it('should return loadCommentsFailure, if the request was unsuccessful', (done: DoneFn) => {
+      actions$ = of(loadComments({ productId }));
+      const errorMessage = 'Some error';
+      axiosGetSpy.and.returnValue({ data: errorMessage, status: 404 });
+      effects.loadComments$.subscribe((action) => {
+        expect(action).toEqual(loadCommentsFailure({ error: new Error(errorMessage) }));
+        done();
+      });
+    });
+
+    it('should return loadCommentsFailure, if the request threw an error', (done: DoneFn) => {
+      actions$ = of(loadComments({ productId }));
+      const errorMessage = 'Some error';
+      axiosGetSpy.and.throwError(errorMessage);
+      effects.loadComments$.subscribe((action) => {
+        expect(action).toEqual(loadCommentsFailure({ error: new Error(errorMessage) }));
+        done();
+      });
+    });
   });
 });
